64da680731fd0eb15ce2e26fe9fc2ed343e6f060
Soumyajit Basak <soumyajitb089@gmail.com>
2025-09-18 00:44:42 +0530

    Update API fetch URL on the login page

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -18,7 +18,7 @@ const LoginPage = ({ setAuth }) => {
         e.preventDefault();
         try {
             const body = { email, password };
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://ecoshakti-8rh9.onrender.com", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
